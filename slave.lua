16c3ce7dd13eba4124ca0b00de1e5198b66beb03
Daniel Nguyen <daniel.nguyen.s173@gmail.com>
2022-06-02 19:55:41 +1000

    broadcoast slave

--- slave.lua
+++ slave.lua
@@ -10,9 +10,20 @@ end
 function send ()
     while true do
         getFloors()
+        print("Please enter a floor...")
         promptFloor()
-        
-        
+        local input = read()
+        local valid = false
+        for i, v in pairs(floors) do
+            if (i == tonumber(input)) then
+                valid = true
+            end
+        end
+        if valid then
+            rednet.broadcast({action='request', floor=tonumber(input)})
+        else
+            print("Invalid selection")
+        end
     end
 end
 
@@ -24,7 +35,7 @@ function getFloors()
             rednet.broadcast({action='getFloors'})
             _, msg = rednet.receive()
         end
-        floors = msg.foors
+        floors = msg.floors
     end
 end
 
